e7d903d0a1a13cde423615c834797c9421a50097
prodgers1 <pcrodgers1@gmail.com>
2024-12-03 06:07:53 +0000

    Day 3

--- Day3/main.py
+++ Day3/main.py
@@ -3,6 +3,35 @@ import math
 from copy import deepcopy
 from collections import defaultdict
 from itertools import pairwise
+import re
 
-input = open("Day2/input.txt").readlines()
-input = [s.rstrip() for s in input]
+input = open("Day3/input.txt").read()
+#input = [s.rstrip() for s in input]
+
+pattern = r'mul\((\d*),(\d*)\)'
+DONT = "don't()"
+DO = "do()"
+
+def find_ans(line):
+  matches =  re.findall(pattern, line)
+  ans = 0
+  for match in matches:
+    ans += (int(match[0]) * int(match[1]))
+    #print(ans)
+  return ans
+
+print(find_ans(input))
+
+ans = 0
+enabled_memory = []
+for match in re.split(r"do\(\)", input):
+  index = match.find(DONT)
+  if index != -1:
+      enabled_memory.append(match[:index])
+  else:
+      enabled_memory.append(match)
+
+for e in enabled_memory:
+   ans += find_ans(e)  
+
+print(ans)
